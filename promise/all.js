cd726cbe38caeb2fba5ad6a29a28a0e9d25eeeb5
zhouxshen <594360560@qq.com>
2025-03-05 12:51:59 +0800

    feat: 重新排列

---
+++ promise/all.js
@@ -0,0 +1,28 @@
+Promise.myAll = (params) => {
+  let res, rej;
+  const p = new Promise((resolve, reject) => {
+    res = resolve;
+    rej = reject;
+  });
+  let i = 0;
+  let fullfilled = 0;
+  const result = [];
+  for (const param of params) {
+    const index = i;
+    i ++
+    Promise.resolve(param).then((data) => {
+      result[index] = data;
+      fullfilled ++;
+      if (fullfilled === i) {
+        res(result);
+      }
+    }).catch(err => rej(err))
+  }
+  if (i === 0) res([])
+  return p
+}
+
+Promise.myAll([1,2,3,4, Promise.reject(1)]).then(
+  res => console.log('success', res),
+  error => console.log('error', error)
+);
